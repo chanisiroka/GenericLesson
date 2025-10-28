1e560c85b64eaa85a4c6eed415bd3c3fddb12e16
chanisiroka <60046354+chanisiroka@users.noreply.github.com>
2025-10-28 22:26:23 +0200

    Add project files.

---
+++ GenericLesson/Check.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericLesson
+{
+    class Check <T>
+    {
+
+        public bool Compare(T var1,T var2) {
+
+            if (var1.Equals(var2))
+                return true;
+            return false;
+
+        }
+    }
+}
---
+++ GenericLesson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericLesson
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Check<int> checkInt = new Check<int>();
+            bool check = checkInt.Compare(3, 4);
+            Console.WriteLine(check);
+
+            Check<string> checkStr = new Check<string>();
+            Console.WriteLine(checkStr.Compare("aaaa", "aaaa"));
+            Console.Read();
+        }
+    }
+}
